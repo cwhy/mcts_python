b1690e0d30c19addaf3868729d033ca4e2f1d270
Chen Yu <chenyu@staff.main.ntu.edu.sg>
2020-07-07 22:41:16 +0800

    improved a bit CLI UX

--- run_mcts.py
+++ run_mcts.py
@@ -1,7 +1,7 @@
 from env_utils import pit, RandomAgent
 from mcts import self_play
 from mcts_agent import MctsAgent
-from my_tictactoe import h, init_state, get_available_actions, model, n_players, CliAgent
+from my_tictactoe import h, init_state, get_available_actions, model, n_players, CliAgent, player_symbols
 from table_utils import TablePolicy
 
 n_actions = h ** 2
@@ -19,17 +19,25 @@ for i in range(n_players):
 for agent in mcts_agents:
     agent.assign_next_(mcts_agents[(agent.ag_id + 1) % n_players])
 
+print("Learning...")
 memory, mcts_agents = self_play(env_model, init_state,
                                  mcts_agents, memory, n_iters, n_eps)
 
 
 
+print("Testing Against Random...")
+print(f"as {player_symbols[0]}")
 for _ in range(5):
     pit(env_model, init_state, [mcts_agents[0], RandomAgent(1, env_model, get_available_actions)])
     print('-----------------------------------------')
 
+print(f"as {player_symbols[1]}")
 for _ in range(5):
     pit(env_model, init_state, [RandomAgent(0, env_model, get_available_actions), mcts_agents[1]])
     print('-----------------------------------------')
 
+print("Testing Against Cli/Human...")
+print(f"as {player_symbols[0]}")
 pit(env_model, init_state, [mcts_agents[0], CliAgent(1, None, None)])
+print(f"as {player_symbols[1]}")
+pit(env_model, init_state, [CliAgent(0, None, None), mcts_agents[1]])
